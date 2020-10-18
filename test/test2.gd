318a04fb92d299894cc9221e95490f7b3dc530ce
bernardcloutier <bernard.p.cloutier@gmail.com>
2020-10-17 21:19:33 -0400

    Experiments with saving/loading scripts in resources.

--- test/test2.gd
+++ test/test2.gd
@@ -5,22 +5,42 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+var my_source: String = """
+extends Resource
+
+var my_var = 0
+
+func say_hello():
+	print(\"Hello! this is my value: %s \" % my_var)
+"""
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+#	var my_resource: Resource = load("res://test/resource.gd").new()
+#	var my_resource2: Resource = load("res://test/resource.gd").new()
+#	my_resource.my_string = "Hello"
+#	my_resource2.my_string = "Goodbye"
+#	my_resource.set_meta("my_custom_var", 1)
+#	my_resource2.set_meta("my_custom_var", 2)
+#	print(my_resource.get_meta("my_custom_var"))
+#	print(my_resource2.get_meta("my_custom_var"))
+#	ResourceSaver.save("res://test/saved_resource.tres", my_resource)
+#	ResourceSaver.save("res://test/saved_resource2.tres", my_resource2)
+#	var my_loaded_resource = load("res://test/saved_resource.tres")
+#	var my_loaded_resource2 = load("res://test/saved_resource2.tres")
+#	print(my_loaded_resource.my_string)
+#	print(my_loaded_resource2.my_string)
+#	print(my_loaded_resource.get_meta("my_custom_var"))
+#	print(my_loaded_resource2.get_meta("my_custom_var"))
 	var my_resource: Resource = load("res://test/resource.gd").new()
-	var my_resource2: Resource = load("res://test/resource.gd").new()
-	my_resource.my_string = "Hello"
-	my_resource2.my_string = "Goodbye"
-	my_resource.set_meta("my_custom_var", 1)
-	my_resource2.set_meta("my_custom_var", 2)
-	print(my_resource.get_meta("my_custom_var"))
-	print(my_resource2.get_meta("my_custom_var"))
+	var my_script = GDScript.new()
+	my_script.source_code = my_source
+	my_script.reload()
+	my_resource.set_script(my_script)
+#	var my_script_instance = my_script.new()
+	my_resource.say_hello()
+	my_resource.my_var = 1
 	ResourceSaver.save("res://test/saved_resource.tres", my_resource)
-	ResourceSaver.save("res://test/saved_resource2.tres", my_resource2)
 	var my_loaded_resource = load("res://test/saved_resource.tres")
-	var my_loaded_resource2 = load("res://test/saved_resource2.tres")
-	print(my_loaded_resource.my_string)
-	print(my_loaded_resource2.my_string)
-	print(my_loaded_resource.get_meta("my_custom_var"))
-	print(my_loaded_resource2.get_meta("my_custom_var"))
+	my_loaded_resource.say_hello()
